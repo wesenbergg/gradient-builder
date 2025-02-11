6613a944d9e5117fe280c2dbb8025bf653a61e4b
Wesenberg <boriss.jerjomkin@novatron.fi>
2025-02-11 22:59:58 +0200

    🌚 Add dark mode toggle and enhance MainLayout styles for improved accessibility

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import MainLayout from "./components/MainLayout.vue";
+
+const isDarkMode = ref(false);
+
+const toggleDarkMode = () => {
+  isDarkMode.value = !isDarkMode.value;
+  document.documentElement.classList.toggle('dark', isDarkMode.value);
+};
 </script>
 
 <template>
+  <button @click="toggleDarkMode"
+    class="absolute top-4 right-4 w-12 h-12 text-2xl bg-cyan-500/60 text-black font-bold rounded-full hover:bg-cyan-500/80 transition-colors">
+    {{ isDarkMode ? '🌝' : '🌚' }}
+  </button>
   <MainLayout msg="Gradient builder" />
 </template>
 
--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -8,7 +8,8 @@ const aspectRatio = ref("aspect-1-1");
 </script>
 
 <template>
-  <main class="flex items-center space-x-16 justify-center bg-gray-50">
+  <main
+    :class="['flex items-center space-x-16 justify-center text-cyan-900 bg-cyan-50 dark:text-cyan-50! dark:bg-cyan-900!']">
     <section class="hidden sm:block space-y-4">
       <div :class="[
         'border-2 border-cyan-950/15 shadow-xl flex flex-col items-center justify-center bg-gray-500 rounded-xl h-[50dvh]',
@@ -19,12 +20,10 @@ const aspectRatio = ref("aspect-1-1");
         <!-- <h1 class="text-4xl font-bold text-gray-800 mb-8">{{ msg }}</h1> -->
       </div>
       <div class="space-x-4">
-        <button class="border border-cyan-800 text-cyan-800 px-4 py-2 rounded-md"
-          @click="() => (aspectRatio = 'aspect-1-1')">
+        <button class="border border-cyan-800 px-4 py-2 rounded-md" @click="() => (aspectRatio = 'aspect-1-1')">
           1:1
         </button>
-        <button class="border border-cyan-800 text-cyan-800 px-4 py-2 rounded-md"
-          @click="() => (aspectRatio = 'aspect-3-4')">
+        <button class="border border-cyan-800 px-4 py-2 rounded-md" @click="() => (aspectRatio = 'aspect-3-4')">
           3:4
         </button>
       </div>
@@ -33,20 +32,26 @@ const aspectRatio = ref("aspect-1-1");
       <div class="max-w-[100vw]">
         <img :src="logo" alt="Logo" class="w-32 h-32 mb-8 rounded-2xl" width="64px" height="64px" />
 
-        <h1 class="text-4xl font-bold text-gray-800 mb-8">{{ msg }}</h1>
+        <h1 class="text-4xl font-bold mb-8">{{ msg }}</h1>
 
         <div class="grid grid-cols-5 gap-32 text-center *:pt-[18px] w-full sm:w-md overflow-auto">
           <span class="w-24 h-24 mb-8 rounded-2xl bg-amber-500"></span>
-          <span class="w-24 h-24 mb-8 rounded-2xl bg-cyan-900/20 text-cyan-900 text-5xl">+</span>
-          <span class="w-24 h-24 mb-8 rounded-2xl bg-cyan-900/20 text-cyan-900 text-5xl">+</span>
-          <span class="w-24 h-24 mb-8 rounded-2xl bg-cyan-900/20 text-cyan-900 text-5xl">+</span>
-          <span class="w-24 h-24 mb-8 rounded-2xl bg-cyan-900/20 text-cyan-900 text-5xl">+</span>
+          <span
+            class="w-24 h-24 mb-8 rounded-2xl dark:bg-cyan-50/20 bg-cyan-900/20 dark:text-cyan-50 text-cyan-900 text-5xl">+</span>
+          <span
+            class="w-24 h-24 mb-8 rounded-2xl dark:bg-cyan-50/20 bg-cyan-900/20 dark:text-cyan-50 text-cyan-900 text-5xl">+</span>
+          <span
+            class="w-24 h-24 mb-8 rounded-2xl dark:bg-cyan-50/20 bg-cyan-900/20 dark:text-cyan-50 text-cyan-900 text-5xl">+</span>
+          <span
+            class="w-24 h-24 mb-8 rounded-2xl dark:bg-cyan-50/20 bg-cyan-900/20 dark:text-cyan-50 text-cyan-900 text-5xl">+</span>
         </div>
       </div>
       <div class="flex items-end space-x-4">
-        <button type="button" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
+        <button type="button"
+          class="px-6 py-2 bg-cyan-400/70 text-black font-bold rounded-full hover:bg-cyan-400 transition-colors">
           Copy CSS</button>
-        <button type="button" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
+        <button type="button"
+          class="px-6 py-2 bg-cyan-400/70 text-black font-bold rounded-full hover:bg-cyan-400 transition-colors">
           Download as Image</button>
       </div>
     </section>
